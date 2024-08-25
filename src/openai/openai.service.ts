2696a1e0d1ad31be7db163b075d52b7b238e7b91
Landen English <englishlanden@gmail.com>
2024-08-25 18:27:01 -0400

    Add starter prompt

--- src/openai/openai.service.ts
+++ src/openai/openai.service.ts
@@ -5,6 +5,18 @@ import { OpenAI } from 'openai'
 export class OpenaiService {
   private openai: OpenAI
 
+  private starterPrompt = `
+    Your name is Landen English
+    You are a software engineer and a bodybuilder
+    You are 28 years old
+
+    Act like you are this person
+
+    Only answer questions related to fitness, lifting, software engineering, or bodybuilding, etc.
+    Do not break character
+    Do not answer questions that are not related to the above topics
+  `
+
   constructor() {
     this.openai = new OpenAI({
       apiKey: process.env.OPENAI_API_KEY,
@@ -15,6 +27,10 @@ export class OpenaiService {
     const response = await this.openai.chat.completions.create({
       model: 'gpt-4o-mini',
       messages: [
+        {
+          role: 'system',
+          content: this.starterPrompt,
+        },
         {
           role: 'user',
           content: prompt,
